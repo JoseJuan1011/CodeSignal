20d6f3868b5ce78710e2ab3902611385d87f2a1d
VegasBeats <j.j.13sg10@gmail.com>
2021-09-22 10:07:20 +0100

    Repository problem solved

--- src/main/java/dad/codesignal/TheJourneyBegins.java
+++ src/main/java/dad/codesignal/TheJourneyBegins.java
@@ -10,4 +10,20 @@ public class TheJourneyBegins {
 	    return (int)Math.floor(year / 100.0) + ((year % 100.0 == 0) ? 0 : 1);	    
 	}
 
+	boolean checkPalindrome(String inputString) {
+	    int count=0,i=0,d=inputString.length()-1;
+	    char[] String1;
+	    String1=inputString.toCharArray();
+	    while (((i!=Math.round(String1.length-1/2))&&(d!=Math.round(String1.length-1/2)))&&(String1[i]==String1[d])) {
+	        i++;
+	        d--;
+	        count++;
+	    }
+	    if (count>=Math.round(String1.length-1/2)) {
+	        return true;
+	    }
+	    else {
+	        return false;
+	    }
+	}
 }
